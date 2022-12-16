da6a63e746d1595a220c703169a7022fa19ee7a1
mezeiser <mezardini@gmail.com>
2022-12-16 09:59:27 +0100

    sk commit

--- dinipredictx/settings.py
+++ dinipredictx/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qyc4b#i99uv11os6@+kn_&-*c$vb+)2bs0ko0ut2e4_4lgi4n-'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
